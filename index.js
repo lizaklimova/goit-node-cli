b115eac970d82214f605a6712faa1a095d9954ff
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-01-12 19:18:30 +0200

    finished

---
+++ index.js
@@ -0,0 +1,44 @@
+const { program } = require("commander");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./src/contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      await listContacts();
+      break;
+
+    case "get":
+      await getContactById(id);
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
